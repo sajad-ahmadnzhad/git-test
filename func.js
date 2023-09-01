e002638156c8bf128c97483b6b76fb7bb14b6866
sajad <ahmadnzhad22>
2023-09-02 00:37:16 +0430

    added new array and function

--- func.js
+++ func.js
@@ -19,3 +19,18 @@ let func = (number) => number**2
 consloe.log(func(12))
 
 let loginUser = () => true
+
+let array = [1,2,3,4,5,6]
+for(let item of array ) {
+    console.log(array[item])
+}
+
+let funcArrow = (array) => {
+    let sum = 0
+    for(let item of array) {
+        sum += item[array] + 10
+    }
+    return sum
+}
+
+console.log(funcArrow(array)) // result [11 , 12 , 13 , 14, 15 , 16]
